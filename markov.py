7441384f67ea1dfb507e42be51f5e4748a31ff63
Hackbright Student <info@hackbrightacademy.com>
2015-04-16 13:51:31 -0700

    Can now add an infinite number of files

--- markov.py
+++ markov.py
@@ -8,13 +8,13 @@ class SimpleMarkovGenerator(object):
 
     def read_files(self, filenames):
         """Given a list of files, make chains from them."""
-        # cache = []
-        # for n in sys.argv[1:]:
-        open_file = open(filenames)
-        open_file = open_file.read().split()
-
-
-        return self.make_chains(open_file)
+        file_list = []
+        for filename in filenames:
+            open_file = open(filename)
+            open_file = open_file.read().split()
+            file_list.append(open_file)
+        
+        return self.make_chains(file_list)
 
 
 
@@ -78,9 +78,12 @@ if __name__ == "__main__":
     # we should make an instance of the class
     # we should call the read_files method with the list of filenames
     # we should call the make_text method 5x
-    
-    script, corpus = sys.argv
+    cache = []
+    # script, corpus = sys.argv
+    for filename in sys.argv[1:]:
+        cache.append(filename)
+
 
     n_gram_length = int(raw_input("How long do you want you n_gram to be > "))
     smg = SimpleMarkovGenerator(n_gram_length)
-    smg.read_files(corpus)
+    smg.read_files(cache)
