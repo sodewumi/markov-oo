0fde443d6a386289c1a33df43b5dff89eac2721f
Hackbright Student <info@hackbrightacademy.com>
2015-04-16 20:36:05 -0700

    Can now check truncate a message to 140 characters

--- markov.py
+++ markov.py
@@ -1,6 +1,9 @@
 import sys
 from random import choice
 
+class BaseMarkov(object):
+    pass
+
 class SimpleMarkovGenerator(object):
     def __init__(self, length):
         self.length = length
@@ -71,6 +74,45 @@ class SimpleMarkovGenerator(object):
 
         return generated_txt
 
+class Twitter(SimpleMarkovGenerator):
+
+    def __init__(self, length):
+        super(Twitter, self).__init__(length)
+    
+    def read(self, filenames):
+        self.read_files(filenames)
+
+    def make_text(self, chains):
+        """Takes dictionary of markov chains; returns random text."""
+
+        # choose a random bi-gram then find the value of said bi-gram. 
+        # Afterward choose a random value from the generated list
+        random_n_gram = choice(chains.keys())
+        random_word = choice(chains[random_n_gram])
+        generated_txt = random_word
+
+        # the last word from the previous bi-gram key along with the randomly
+        # choosen word is saved for the next iteration of the while loop
+        next_tuple = random_n_gram[1:] + tuple([random_word])
+        n_gram_value = chains.get(next_tuple)
+
+        while n_gram_value and len(generated_txt) < 141:
+            # the random word is put in the first element of the nxt word tuple,
+            # and the second element is the newly choosen random word.
+            new_random_word = choice(n_gram_value)
+            if len(generated_txt +" "+ new_random_word) < 141:
+                generated_txt = generated_txt +" "+ new_random_word
+                next_tuple = next_tuple[1:] + tuple([new_random_word])
+            else:
+                break
+
+            # if the bi-gram from the previous while loop has a value, return it
+            # If there is not a value, return None and close the loop
+            n_gram_value = chains.get(next_tuple)
+
+
+        return generated_txt
+
 
 
 if __name__ == "__main__":
@@ -79,12 +121,16 @@ if __name__ == "__main__":
     # we should make an instance of the class
     # we should call the read_files method with the list of filenames
     # we should call the make_text method 5x
-    # cache = []
-    # # script, corpus = sys.argv
-    # for filename in sys.argv[1:]:
-    #     cache.append(filename)
-
 
     n_gram_length = int(raw_input("How long do you want you n_gram to be > "))
-    smg = SimpleMarkovGenerator(n_gram_length)
-    smg.read_files(sys.argv[1:])
+
+    punctuation = raw_input("Do you want to check for punctuation? Yes or No > ")
+
+    twitter = raw_input("do you want a twitter bot?")
+
+    if twitter == "yes":
+        mwp = Twitter(n_gram_length)
+        mwp.read(sys.argv[1:])
+    elif twitter == "no":  
+        smg = SimpleMarkovGenerator(n_gram_length)
+        smg.read_files(sys.argv[1:])
